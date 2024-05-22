b9eaf2342b870b179fdc97ed4d7d015167f02e80
lucianchickenlover <avoidall99@outlook.com>
2024-05-22 22:25:23 +0200

    XDDD2

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,2 @@
 /// <reference types="@sveltejs/kit" />
 
-declare module '*sites.yml' {
-  export const lastParsed: string
-  const sites: import('./lib').Site[]
-  export default sites
-}
-
-declare module '*tools.yml' {
-  const links: Record<string, string>
-  export default links
-}
--- src/tasks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fetch-github-metadata'
-export * from './screenshots'
-export * from './update-readme'
-
-export const action_types = [`add-missing`, `update-existing`] as const
-export type Action = (typeof action_types)[number]
